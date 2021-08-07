4cf5ab81285d94b74dc67fa3d1d79aafc5c10d70
Chase <pengxu1993@gmail.com>
2021-08-06 17:37:16 -0700

    basic structure and concepts

--- engine.js
+++ engine.js
@@ -1,27 +1,44 @@
-import {update as updateSnake, draw as drawSnake, SNAKE_SPEED} from './snake.js';
+import {update as updateSnake, draw as drawSnake, SNAKE_SPEED, getSnakeHead, snakeIntersection} from './snake.js';
+import { update as updateFood, draw as drawFood,  } from './treat.js';
+import { outsideGrid } from './grid.js';
 
-let lastRenderTime = 0;
+var lastRenderTime = 0;
 const board = document.getElementById('board')
-
+let gameOver = false
+console.log('run')
 
 function main(currentTime){
-    const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
-    if(secondsSinceLastRender < 1 / SNAKE_SPEED) return 
+    if(gameOver){
+        if(confirm('You lost. Press ok to restart.')){
+            window.location ='/'
+        }
+        return
+    }
+
     window.requestAnimationFrame(main);
+    const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
+    if(secondsSinceLastRender < 1 / SNAKE_SPEED) return;
     lastRenderTime = currentTime;
-    console.log(secondsSinceLastRender);
-
-    // update();
-    // draw();
-
+    // console.log(secondsSinceLastRender);
+    update();
+    draw();
 }
 
 window.requestAnimationFrame(main);
 
 function update(){
     updateSnake()
+    updateFood()
+    checkDeath()
 }
 
 function draw(){
+    board.innerHTML = ''
     drawSnake(board)
+    drawFood(board)
+}
+
+function checkDeath(){
+    gameOver = outsideGrid(getSnakeHead()) || snakeIntersection()
+
 }
